e2a51639a161ddf8b12894ce8d5d87403802e625
Lamine <lamine.diakite@lojelis.com>
2020-11-04 16:37:52 +0100

    Initial Commit

---
+++ home-clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  home-clone
+//
+//  Created by Lamine DIAKITE on 04/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ home-clone/home_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  home_cloneApp.swift
+//  home-clone
+//
+//  Created by Lamine DIAKITE on 04/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct home_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
